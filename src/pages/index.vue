88e7f6f119599aee6291a9962ffdcf821b86b716
Jang Haemin <io@jhaemin.com>
2019-10-08 20:28:27 +0900

    Create home component

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,16 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <h1>webglue</h1>
+  </div>
 </template>
 
-<script>
-export default {}
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  head() {
+    return {
+      title: 'webglue'
+    }
+  }
+})
 </script>
