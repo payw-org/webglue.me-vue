73c94602fded65f26de60d3fa578e316c651f6db
Jang Haemin <io@jhaemin.com>
2019-10-08 21:01:28 +0900

    Add Google login button

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h1>webglue</h1>
+    <div class="g-signin2" data-onsuccess="onSignIn"></div>
   </div>
 </template>
 
@@ -9,7 +9,21 @@ import Vue from 'vue'
 export default Vue.extend({
   head() {
     return {
-      title: 'webglue'
+      title: 'webglue',
+      script: [
+        {
+          src: 'https://apis.google.com/js/platform.js',
+          async: true,
+          defer: true
+        }
+      ],
+      meta: [
+        {
+          name: 'google-signin-client_id',
+          content:
+            '560957305620-o5ki7i71ds1iv4uaefrumpeanefep8g7.apps.googleusercontent.com.apps.googleusercontent.com'
+        }
+      ]
     }
   }
 })
