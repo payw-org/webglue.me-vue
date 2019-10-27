9191431d783f83029a85a398bee132ca4b48cdf1
Jang Haemin <io@jhaemin.com>
2019-10-27 21:26:18 +0900

    Redesign sign in button, add appearing transition

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,7 +3,7 @@
     <div class="login-container">
       <div class="logo-wrapper">
         <div class="logo-container">
-          <div class="logo-image">
+          <div class="logo-image" :class="{ appear: isMounted }">
             <img src="~/assets/images/logo-simplified.svg" />
           </div>
           <div class="logo-text">
@@ -11,11 +11,11 @@
           </div>
         </div>
       </div>
-      <div class="login-btn">
+      <div class="login-btn" :class="{ appear: isMounted }">
         <a href="https://api.dev-hch.webglue.me/v1/oauth2/google">
           <button class="google-login">
             <img src="~/assets/images/google_logo.png" class="google-logo" />
-            <div>Sign in with Google</div>
+            <p class="text">Sign in with Google</p>
           </button>
         </a>
       </div>
@@ -34,6 +34,14 @@ export default {
     return {
       title: 'webglue'
     }
+  },
+  data() {
+    return {
+      isMounted: false
+    }
+  },
+  mounted() {
+    this.isMounted = true
   },
   methods: {
     getUserInfo() {
@@ -86,29 +94,54 @@ export default {
     text-align: center;
     transform: translate(-50%, -50%);
   }
+
   .logo-image {
     width: 70vw;
     display: block;
     margin: auto;
     max-width: 20rem;
+    opacity: 0;
+    transform: scale(20);
+
+    &.appear {
+      $time: 3s;
+      $curve: cubic-bezier(0.5, 0.03, 0, 1.04);
+      transition: opacity $time/2 $curve, transform $time $curve;
+      opacity: 1;
+      transform: scale(1);
+    }
   }
+
   .login-btn {
     width: 100vw;
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: center;
-    padding-top: 4rem;
+    padding-top: f(2);
+    opacity: 0;
+
+    &.appear {
+      animation: springSlideUp 1s linear both;
+      animation-delay: 3s;
+    }
+
     .google-login {
-      background-color: #e9e8e8;
-      padding: 1rem;
+      background-color: #fff;
+      padding: s(4) s(5);
       display: flex;
       flex-direction: row;
-      border-radius: 0.84rem;
+      border-radius: r(2);
+      box-shadow: 0 0.15rem 0.3rem rgba(#000, 0.2);
       align-items: center;
+
       .google-logo {
-        width: 1rem;
-        margin-right: 0.3rem;
+        width: 1.2rem;
+        margin-right: s(3);
+      }
+
+      .text {
+        font-size: b(4);
       }
     }
   }
