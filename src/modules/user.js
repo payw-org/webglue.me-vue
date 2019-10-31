407706d777750eb96d301356621a9cd59cf82df7
Jang Haemin <io@jhaemin.com>
2019-10-31 13:34:57 +0900

    Create user util

---
+++ src/modules/user.js
@@ -0,0 +1,19 @@
+import Axios from 'axios'
+import ApiUrl from '~/modules/api-url'
+
+export default class User {
+  static async getUserInfo() {
+    let error
+    const res = await Axios.get(ApiUrl.user.profile, {
+      withCredentials: true
+    }).catch((err) => {
+      error = err
+    })
+
+    if (error) {
+      return undefined
+    } else {
+      return res.data
+    }
+  }
+}
