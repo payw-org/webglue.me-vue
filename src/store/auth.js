c35580a54148eabb80c5d6057c24626bf4bc8389
Jang Haemin <io@jhaemin.com>
2019-10-31 13:37:06 +0900

    Change store 'app' to 'auth'

---
+++ src/store/auth.js
@@ -0,0 +1,24 @@
+export const state = () => ({
+  userInfo: undefined,
+  isSignedIn: false
+})
+
+export const mutations = {
+  setUser(state, data) {
+    state.userInfo = data
+  },
+  setSignedIn(state, bool) {
+    state.isSignedIn = bool
+  }
+}
+
+export const actions = {
+  signIn({ commit }, userInfo) {
+    commit('setUser', userInfo)
+    commit('setSignedIn', true)
+  },
+  clear({ commit }) {
+    commit('setUser', undefined)
+    commit('setSignedIn', false)
+  }
+}
