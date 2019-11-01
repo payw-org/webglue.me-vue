3dfb07fec345a064c599e571e497be46da5032f8
Jang Haemin <io@jhaemin.com>
2019-11-01 19:12:52 +0900

    Add mirroring api url

--- src/modules/api-url.js
+++ src/modules/api-url.js
@@ -2,5 +2,6 @@ export default {
   user: {
     signInUp: 'https://api.dev.webglue.me/v1/oauth2/google',
     profile: 'https://api.dev.webglue.me/v1/me/profile'
-  }
+  },
+  mirror: 'https://api.dev.webglue.me/v1/mirroring/html'
 }
