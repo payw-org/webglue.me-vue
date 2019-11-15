52c4d2e72a8d6bd229a7236f5fc23000d56c9ba8
Jang Haemin <io@jhaemin.com>
2019-11-15 21:06:24 +0900

    Create color definition

---
+++ src/modules/color.js
@@ -0,0 +1,19 @@
+const color = {
+  color1: 'e6696e',
+  color2: 'e8765c',
+  color3: 'dd8042',
+  color4: 'd69d2f',
+  color5: '6ca64e',
+  color6: '5ba68f',
+  color7: '629eb1',
+  color8: '6c92c3',
+  color9: '737ec4',
+  color10: '9177c0',
+  color11: 'c485c6',
+  color12: 'dd7190',
+  color13: '87919d',
+  color14: '8d928e',
+  color15: '9c908b'
+}
+
+export default color
