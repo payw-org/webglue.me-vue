c83efac654c8f68c7821c8ac05df2405ad8ae599
Jang Haemin <io@jhaemin.com>
2019-11-23 02:49:44 +0900

    test: create mirror api on express router

---
+++ src/server/router.js
@@ -0,0 +1,20 @@
+import apiUrl from '../modules/api-url'
+const express = require('express')
+const Axios = require('axios').default
+
+const router = express.Router()
+
+router.get('/mirror', (req, response) => {
+  // form: /mirror?url=
+  const url = req.url
+  const mirroringUrl = url.replace(/^\/mirror\?url=/, '')
+  Axios({
+    url: apiUrl.mirror(mirroringUrl),
+    method: 'get',
+    withCredentials: true
+  }).then(res => {
+    response.send(res.data)
+  })
+})
+
+module.exports = router
