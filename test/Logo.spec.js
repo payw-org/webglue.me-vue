da98f1e361275bf8ed2359fcb275c2be0be075f0
Jang Haemin <io@jhaemin.com>
2019-11-27 17:15:11 +0900

    test: use mocha and chai

--- test/Logo.spec.js
+++ test/Logo.spec.js
@@ -1,9 +1,4 @@
 import { mount } from '@vue/test-utils'
 import Logo from '@/components/Logo.vue'
-
-describe('Logo', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(Logo)
-    expect(wrapper.isVueInstance()).toBeTruthy()
-  })
-})
+import { describe } from 'mocha'
+import { expect } from 'chai'
